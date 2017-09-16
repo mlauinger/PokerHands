3a3e1f313b01a8f5cf68b277cdf8409feb986994
Marco Lauinger <marco.lauinger@gmail.com>
2017-09-16 18:33:11 +0200

    added getter; use new input parsing

--- src/main/java/de/mlauinger/pokerhands/model/Card.java
+++ src/main/java/de/mlauinger/pokerhands/model/Card.java
@@ -5,9 +5,16 @@ public class Card {
     private CardSuit cardSuit;
     private CardValue cardValue;
 
-    public Card(String suitString, String valueString) {
-        this.cardSuit = CardSuit.valueOf(suitString);
-        this.cardValue = CardValue.valueOf(valueString);
+    public Card(String suitString, String valueString) throws InvalidInputException {
+        this.cardSuit = CardSuit.parseValue(suitString);
+        this.cardValue = CardValue.parseValue(valueString);
     }
 
+    public CardSuit getCardSuit() {
+        return cardSuit;
+    }
+
+    public CardValue getCardValue() {
+        return cardValue;
+    }
 }
