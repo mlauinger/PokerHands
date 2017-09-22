5cb3992ca016cff834544a7a8d4517808d93661d
Marco Lauinger <marco.lauinger@gmail.com>
2017-09-22 22:55:55 +0200

    replaced old counting system

--- src/main/java/de/mlauinger/pokerhands/model/Hand.java
+++ src/main/java/de/mlauinger/pokerhands/model/Hand.java
@@ -5,37 +5,30 @@ import java.util.*;
 public class Hand {
 
     private List<Card> cards;
-    private HashMap<CardSuit, Integer> suitsInHand;
-    private HashMap<CardValue, Integer> valuesInHand;
     private List<ValueCount> valueCounts = new ArrayList<>(3);
     private List<SuitCount> suitCounts = new ArrayList<>(4);
 
     public Hand() {
         cards = new ArrayList<>();
-        initSuitMap();
-        initValueMap();
     }
 
-    private void initValueMap() {
-        valuesInHand = new HashMap<>();
-        Arrays.asList(CardValue.values()).forEach(value -> {
-            valuesInHand.put(value, 0);
-        });
+
+    int getCount(CardSuit suit) {
+        for (SuitCount count : suitCounts) {
+            if (count.getValue().equals(suit)) {
+                return count.getCount();
+            }
+        }
+        return 0;
     }
 
-    private void initSuitMap() {
-        suitsInHand = new HashMap<>();
-        Arrays.asList(CardSuit.values()).forEach(suit -> {
-            suitsInHand.put(suit, 0);
-        });
-    }
-
-    public int getCount(CardSuit suit) {
-        return suitsInHand.get(suit);
-    }
-
-    public int getCount(CardValue value) {
-        return valuesInHand.get(value);
+    int getCount(CardValue value) {
+        for (ValueCount count : valueCounts) {
+            if (count.getValue().equals(value)) {
+                return count.getCount();
+            }
+        }
+        return 0;
     }
 
     public void addCard(Card card) {
@@ -44,20 +37,18 @@ public class Hand {
     }
 
     private void incrementCounts(Card card) {
-        suitsInHand.put(card.getCardSuit(), suitsInHand.get(card.getCardSuit()) + 1);
-        valuesInHand.put(card.getCardValue(), valuesInHand.get(card.getCardValue()) + 1);
         for (ValueCount count : valueCounts) {
             if (count.getValue().equals(card.getCardValue())) {
                 count.increment();
-                return;
+                break;
             }
         }
         valueCounts.add(new ValueCount(card.getCardValue(), 1));
 
         for (SuitCount count : suitCounts) {
-            if (count.getValue().equals(card.getCardValue())) {
+            if (count.getValue().equals(card.getCardSuit())) {
                 count.increment();
-                return;
+                break;
             }
         }
         suitCounts.add(new SuitCount(card.getCardSuit(), 1));
